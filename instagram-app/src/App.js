cad75773a07a1a160faecc7a8afea9e514f7fa02
Blake Mitchell <mblakeandrew@gmail.com>
2019-06-04 14:14:51 -0700

    began creating the basic file structure and passing props

---
+++ instagram-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import dummyData from "./dummy-data.js";
+import PostContainer from "./Components/PostContainer/PostContainer";
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dummy: dummyData
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <PostContainer dummy={this.state.dummy} />
+      </div>
+    );
+
+  }
+ 
+}
+
+export default App;
---
+++ instagram-app/src/Components/PostContainer/PostContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PostContainer = props => {
+    return (
+        <>
+            {props.dummy.map(dummy => {
+                return (
+                    <div>
+                        hh
+                    </div>
+                );
+            })}
+        </>
+    );
+}
+
+export default PostContainer;
